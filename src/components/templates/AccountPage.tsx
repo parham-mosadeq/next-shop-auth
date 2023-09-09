f304a345b48a66631c3ce4433b839993d5b11170
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-09-10 01:39:01 +0430

    add comment

--- src/components/templates/AccountPage.tsx
+++ src/components/templates/AccountPage.tsx
@@ -2,9 +2,12 @@ import Link from 'next/link';
 import AccountFrom from '../modules/AccountFrom';
 
 export default function AccountPage() {
+
+  // ! make it conditional => if the user is signed in show **dashboard** 
+  // !  if not show log in form
   return (
     <article className='text-center h-full flex justify-center flex-col'>
-      <AccountFrom btnTxt='Login' />
+      <AccountFrom btnTxt='Login' handleSubmit={() => {}} />
       <p className='capitalize font-bold'>
         don't have an account ?{' '}
         <Link className='text-blue-700 font-normal' href='/account/signup'>
