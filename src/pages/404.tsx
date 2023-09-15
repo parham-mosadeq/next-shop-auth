df838f13a4e12d1045fd4724b872407b8a958a1f
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-09-16 01:01:26 +0430

    create 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Notfound() {
+  return (
+    <div className='flex justify-center items-center flex-col h-full capitalize text-2xl'>
+      <h1 className='h-[100px] my-10'>the page your looking for isn't here :(</h1>
+      <p className='h-[100px] my-10'>pleas try again</p>
+    </div>
+  );
+}
